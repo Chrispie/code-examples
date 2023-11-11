0c489e81dc675f5037f94ab0ae0f8ad2fddc52d6
chrispie <chrispie123@gmail.com>
2023-11-11 23:07:21 +0100

    Remove SO

--- so-questions/crossorigin-with-oauth/angular-base-app/src/app/app.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Token {
-  access_token: string,
-  token_type: string;
-  refresh_token: string;
-  expires_in: number;
-  scope: string;
-}
-
-export const TOKEN_NAME = 'access_token';
--- so-questions/crossorigin-with-oauth/oauth-server/src/main/java/com/my/dao/OAuthDAOService.java
+++
@@ -1,9 +0,0 @@
-package com.my.dao;
-
-
-import com.my.model.UserEntity;
-
-public interface OAuthDAOService {
-
-	public UserEntity getUserDetails(String emailId);
-}
